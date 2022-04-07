64b95988aee636f27e99b92e4213d63c26913d2c
sheraz <sherazakram372@gmail.com>
2022-04-07 20:23:33 +0500

    fundamental part-1

--- js crash course/Fundamental Part-1/finall/script.js
+++ js crash course/Fundamental Part-1/finall/script.js
@@ -1,3 +1,4 @@
+/*
 //////////////////////
 // linking  javascript file
 /*let js = 'amazing';
@@ -12,6 +13,7 @@ myHeading.style.border = '1px solid Blue';*/
 
 //////////////////////
 //Data Types
+/*
 let myAge = 20;
 console.log(myAge); 
 console.log(typeof myAge);
@@ -49,10 +51,151 @@ var weight = 50;
 weight = 60;
 alert('my weight is' + ' ' + weight);
 
+
 //creating a const to save my name the value of this const can't be changed
-/*const name = sheraz;
-name = akram;
+const myName = sheraz;
+myName = akram;
 alert('my name is' + ' ' + name);
-*/
+
 const newName = 'ali'
 alert('Name is' + ' ' + newName);
+*/
+
+///////////////////////////////////////////////
+//alert('Hello world');
+/*
+let js = 'amazing';
+if(js === 'amazing') alert('javascript is fun');
+
+js = 'boring';
+if(js === 'boring') alert('javascript is fun');
+
+///////////////////////////////////////////////
+//values and  variables
+
+//name convention
+let firstName = 'sheraz';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let $function = 'programmer';
+console.log($function);
+
+let Person = 'akram';
+console.log(Person);
+
+//variable name cannot start with a number
+let 1person = 'akram';
+console.log(1Person);
+
+
+//////////////////////////////////////
+// data types
+let year = 2002;
+console.log(year);
+console.log(typeof year);
+
+let firstName = 'sheraz';
+console.log(firstName);
+console.log(typeof firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let cars;
+console.log(cars);
+console.log(typeof cars);
+
+console.log(typeof null);
+
+
+//////////////////////////////////
+//let var or const
+
+//let value can be change
+let age = 20;
+age = 25;
+
+//const value cannot be change
+const temp = 35;
+temp = 25;
+
+//var variable similar to let
+var weather = 'summer';
+weather = 'winter';
+
+//////////////////////////////////
+// Basic operator
+
+// Mathematics operator
+const now = 2040;
+const ageSheraz = now - 2002;
+const ageArsal = now - 2015;
+console.log(ageSheraz, ageArsal);
+
+const firstName = 'sheraz';
+const lastName = 'akram';
+console.log(firstName + ' ' + lastName);
+
+//assignment operator
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x ++;
+x --;  
+console.log(x);
+
+//comparison operator
+
+console.log(ageSheraz > ageArsal);
+console.log(ageArsal > ageSheraz);
+
+const isFullAge = ageArsal >= 15;
+console.log(isFullAge >= 15);
+
+console.log(now - 2002 > now - 2015);
+
+////////////////////////////////////////
+// oerator precedence
+
+let x = y;
+x = y = 25 - 10 - 5;
+ console.log(x, y);
+
+ const now = 2040;
+ const ageSheraz = now - 2002;
+ const ageArsal = now - 2015;
+ const averageAge = ageSheraz + ageArsal / 2;
+console.log(ageSheraz, ageArsal, averageAge);
+
+///////////////////////////////////////////////
+// #1 Coding challenge
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.88;
+
+const markBMI = massMark / heightMark ** 2;
+const johnBMI = massJohn / (heightJohn * heightJohn);
+const markHeigherBmi = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHeigherBmi);
+
+///////////////////////////////////////////////
+// string Template and literals
+const newName = 'Hussain';
+const birth = 20;
+const work = 'student';
+
+const sheraz = " i'm " + newName + ' ' + birth + ' year old ' + work;
+console.log(sheraz); 
+
+const newSheraz = `iam ${newName}, a ${birth} year old ${work}`;
+console.log(newSheraz);
+*/
+
+
+
+
