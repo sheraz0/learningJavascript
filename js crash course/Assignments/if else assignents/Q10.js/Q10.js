a5b28038ae0bd3d10e32784af1553d6cc59791fe
sheraz <sherazakram372@gmail.com>
2022-04-12 22:46:55 +0500

    Assignment no:10

---
+++ js crash course/Assignments/if else assignents/Q10.js/Q10.js
@@ -0,0 +1,22 @@
+/*
+10. 01/01/1900, the day was Monday. If any year is input through keyboard, 
+write a program to find out the day on first January of that year?
+*/
+
+let year;
+
+year = Number(prompt(`Enter a year`));
+console.log(typeof year);
+
+if(year === 01/01/1900){
+    alert(`Monday`);
+}
+else if(year === 01/01/1901){
+    alert(`Friday`);
+}
+else if(year === 01/01/1902){
+    alert(`Saturday`);
+}
+else{
+    alert(`invalid input`);
+}
