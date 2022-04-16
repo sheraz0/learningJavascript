a13aad99d157793b71f6469de565f319c08ee5f5
sheraz <sherazakram372@gmail.com>
2022-04-16 21:30:28 +0500

    practices

---
+++ js crash course/Assignments/if else assignents/Q8.js
@@ -0,0 +1,19 @@
+/*
+  8. Write a program that takes the length of two legs of a right triangle 
+  as input and calculates the area and hypotenuse?
+  */
+
+ let a;
+ let b;
+
+ a = prompt('enter a value one');
+ b = prompt('enter a second value');
+
+ if(a >= 50){
+   alert( (a * a) + (b * b) );
+ }
+ else if(b <= 50){
+   alert( (a * a) + (b * b));
+ }
+
+
