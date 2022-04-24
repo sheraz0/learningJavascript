d3a083dedef7569ce3b4ad3aeadfabc070461fd9
sheraz <sherazakram372@gmail.com>
2022-04-24 21:53:00 +0500

    practices

---
+++ js crash course/Assignments/if else assignments/Q9.js
@@ -0,0 +1,30 @@
+/*
+ 9. A pharmaceutical company pays 10% house rent and 90% daily allowance of basic salary
+ to its employees if the basic pay of the employee is less than 1500.
+ The company pays 20% house rent and 95% daily allowance of basic salary to 
+ its employees if the basic pay of the employee is greater than or equal to 1500. Write an algorithm 
+ that takes basic salary of an employee and calculate and prints his gross salary?
+    
+*/
+
+
+let hR;
+let dA;
+let bS;
+let gS;
+
+hR = prompt('Enter a house rent');
+dA = prompt('Enter a Daily Allowance');
+bS = prompt('Enter a Basic Salary');
+
+if(bS >= 1500){
+  gS = (hR / dA) + bS;
+}else if(bS < 1500){
+  gS = (hR / dA) + bS;
+}else{
+  alert('please enter a valid information');
+}
+
+if(gS){
+  alert(`Gross Salary is : ${gS}`);
+}
