9d96faec4c0679fae89abbc57b431fb1d15e75d3
sheraz <sherazakram372@gmail.com>
2022-05-02 22:28:05 +0500

    practices

--- js crash course/Assignments/if else assignments/Q5.js
+++ js crash course/Assignments/if else assignments/Q5.js
@@ -3,23 +3,40 @@
 three numbers using multiple-if or nested-if statement?
 */
 
-let number1;
-let number2;
-let number3;
-let result;
+ let number1;
+ let number2;
+ let number3;
+ let result;
 
-number1 = Number(prompt('Enter number one'));
-number2 = Number(prompt('Enter number two'));
-number3 = Number(prompt('Enter number three'));
+ number1 = Number(prompt('Enter number one'));
+ number2 = Number(prompt('Enter number two'));
+ number3 = Number(prompt('Enter number three'));
 
-if(number1 > number2 && number1 > number3){
-  result = number1;
+ if(number1 > number2 && number1 > number3){
+   result = number1;
+   alert(`${number1} is maximum`);
+ }
+ if(number1 < number2 && number1 < number3){
+   result = number1;
+   alert(`${number1} is minimum`);
+ }
   
-  if(number3 < number1 && number3 < number2)
-    result = number3;
-}
+   if(number2 > number1 && number2 > number3){
+   result = number2;
+   alert(`${number2} is maximum`);
+ }
+ if(number2 < number1 && number2 < number3){
+   result = number2;
+   alert(`${number2} is minimum`);
+ }
+  
+   if(number3 > number1 && number3 > number2){
+   result = number3;
+   alert(`${number3} is maximum`);
+ }
+ if(number3 < number1 && number3 < number2){
+   result = number3;
+   alert(`${number345} is minimum`);
+ }
 
-if(result){
-alert(`${number1} is maximum and ${number3} is minimum`);
-}
 
