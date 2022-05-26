ca3bae448cd3e28af181a00ce9891f4e2c54c9c2
sheraz <sherazakram372@gmail.com>
2022-05-26 13:18:34 +0500

    p17

--- js crash course/Assignments/loops assignments/Q2.js
+++ js crash course/Assignments/loops assignments/Q2.js
@@ -1,11 +1,16 @@
-/*
+
+  /*
    2. Write an program that uses for loop statement to calculate
    and print the product of odd integers from 10-20
-*/
+  */
+
+ let product = 1;
+
+    for(let a = 10; a <= 25; a++){
+      if(a % 2 === 1){
+        product = product * a;
+      }
+    }
+    console.log(`product is : ${product}`);
 
  
-for(let a = 10; a <= 20; a++){
-    if(a % 2 != 0){
-      console.log(a);
-    }
-  }
--- js crash course/Assignments/loops assignments/Q3.js
+++ js crash course/Assignments/loops assignments/Q3.js
@@ -3,10 +3,15 @@
  and print the product of odd integers from 1-25 which are divisible by 5?
 */
 
- 
+let product = 1;
+
 for(let a = 1; a <= 25; a++){
-    if(a % 2 != 0){
-      console.log(`${a} = ${a / 5}`);
-    }
+  if(a % 2 === 1 && a % 5 === 0){
+    product = product * a; 
   }
+}
+console.log(`product is : ${product}`);
+
+ 
+
   
